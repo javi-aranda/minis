c3ef329cd0f7c7023deb572649d2c8ad39003138
javisenberg <javier.aranda.designs@gmail.com>
2022-05-07 20:56:09 +0200

    Loading bar bash script

---
+++ loading_bar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for ((k = 0; k <= 10 ; k++))
+do
+    echo -n "[ "
+    for ((i = 0 ; i <= k; i++)); do echo -n "###"; done
+    for ((j = i ; j <= 10 ; j++)); do echo -n "   "; done
+    v=$((k * 10))
+    echo -n " ] "
+    echo -n "$v %" $'\r'
+    sleep 0.075
+done
+echo
